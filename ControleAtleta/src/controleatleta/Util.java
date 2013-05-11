d1d4852182d18f89416fbe2bafa5a4eaffe4e5e2
lucaskanashiro <lucas-kanashiro@hotmail.com>
2013-05-10 23:48:32 -0300

    Adicionando classe Util para armazenar constantes

---
+++ ControleAtleta/src/controleatleta/Util.java
@@ -0,0 +1,43 @@
+package controleatleta;
+
+public class Util {
+    
+    public Util(){}
+    
+    public static final int L2 = 2;
+    public static final char DESTRO = 'D';
+    public static final int L0 = 0;
+    public static final int L1 = 1;
+    public static final int SLICE = 7;
+    public static final char SEXO_FEMININO_VALOR = 'F';
+    public static final int DESTRO_INDICE = 0;
+    public static final char TIPO_QUADRA_GRAMA = 'G';
+    public static final int DRIVE = 1;
+    public static final char TIPO_QUADRA_PISO_DURO = 'P';
+    public static final int LOB = 4;
+    public static final int TIPO_QUADRA_SAIBRO_INDICE = 0;
+    public static final char SEXO_MASCULINO_VALOR = 'M';
+    public static final byte SEXO_MASCULINO_INDICE = 0;
+    public static final int TIPO_QUADRA_PISO_DURO_INDICE = 1;
+    public static final int OVERHEAD = 5;
+    public static final char TIPO_QUADRA_SAIBRO = 'S';
+    public static final int TOPSPIN = 9;
+    public static final int BACKHAND = 0;
+    public static final char CANHOTO = 'C';
+    public static final int TIPO_QUADRA_GRAMA_INDICE = 2;
+    public static final int L5 = 5;
+    public static final int SMASH = 8;
+    public static final int DROPSHOT = 2;
+    public static final int FOREHAND = 3;
+    public static final int L4 = 4;
+    public static final int PASSINGSHOT = 6;
+    public static final byte SEXO_FEMININO_INDICE = 1;
+    public static final int CANHOTO_INDICE = 1;
+    public static final int L3 = 3;
+    public static final char MAO_PREFERIDA_DIREITA = 'D';
+    public static final int MAO_PREFERIDA_DIREITA_INDICE = 0;
+    final static char MAO_PREFERIDA_ESQUERDA = 'E';
+    final static int MAO_PREFERIDA_ESQUERDA_INDICE = 1;
+    
+    
+}
